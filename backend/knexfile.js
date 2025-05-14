69c4bd1dff5a262844b945c0e91a8ae83b7b8212
Luigi <morandinilouis@gmail.com>
2025-05-14 11:51:52 +0900

    update DB URL

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -11,6 +11,7 @@ module.exports = {
       database: process.env.DB_NAME,
       user:     process.env.DB_USER,
       password: process.env.DB_PASSWORD,
+      url: process.env.DB_URL,
     },
     pool: {
       min: 2,
