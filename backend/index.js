b220dd534c5be49359b56631f5a47a1629f77240
Luigi <morandinilouis@gmail.com>
2025-05-14 11:22:49 +0900

    Updatetd cors

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,12 @@ const allowedOrigins = [
 const app = express();
 
 
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use(express.json());
